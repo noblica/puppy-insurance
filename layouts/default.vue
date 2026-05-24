1a86fd3f647ab2c42e8ca930e1689db4e7beecc4
Dusan Perkovic <pljeskavicica@gmail.com>
2026-05-24 19:00:25 +0200

    Fix image scaling with form

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="n:flex n:min-h-dvh">
     <aside
-      class="n:hidden n:s:flex n:s:w-[45%] n:overflow-hidden"
+      class="n:hidden n:s:flex n:s:w-[45%] n:s:h-dvh n:overflow-hidden n:s:sticky n:s:top-0"
       aria-hidden="true"
     >
       <img src="~/assets/logo.png" alt="" class="n:w-full n:h-full n:object-cover" />
