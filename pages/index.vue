d42943498e9df861db790ad29eb3fbacaa13a612
Dusan Perkovic <pljeskavicica@gmail.com>
2026-05-25 04:30:58 +0200

    fix: add missing Nord component imports to signup page after removing them from composable

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,9 @@
 import "@nordhealth/components/lib/Card";
 import "@nordhealth/components/lib/Stack";
 import "@nordhealth/components/lib/Icon";
+import "@nordhealth/components/lib/Input";
+import "@nordhealth/components/lib/Checkbox";
+import "@nordhealth/components/lib/Button";
 import PasswordStrengthChecklist from "~/components/PasswordStrengthChecklist.vue";
 import { useSignupForm } from "~/composables/useSignupForm";
 
